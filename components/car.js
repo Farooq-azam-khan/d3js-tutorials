3a6d48db71d61c45775e8c230d36919bc06c1406
farooq azam khan <farooq1.khan@ryerson.ca>
2019-12-21 12:31:04 -0500

    todo next: d3 visualization

--- components/car.js
+++ components/car.js
@@ -1,35 +1,5 @@
 export const formatCar = car =>
   `${car.year} ${car.make} ${car.model}: ${car.price}`;
 
-export const carData = [
-  {
-    make: "Nissan",
-    model: "Leaf",
-    year: 2012,
-    price: 1800
-  },
-  {
-    make: "Ford",
-    model: "F150",
-    year: 2009,
-    price: 1950
-  },
-  {
-    make: "Chevrolet",
-    model: "Trailblazer",
-    year: 2009,
-    price: 2100
-  },
-  {
-    make: "Nissan",
-    model: "Leaf",
-    year: 2012,
-    price: 1800
-  },
-  {
-    make: "Nissan",
-    model: "Leaf",
-    year: 2012,
-    price: 1800
-  }
-];
+export const filterByPrice = (cars, price) =>
+  cars.filter(car => car.price < price);
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import { formatCar } from "./components/car.js";
+import { formatCar, filterByPrice } from "./components/car.js";
 
-d3.json("./data/cars.json").then(
-  data => (document.getElementById("carsData").innerText = JSON.stringify(data))
-);
+d3.json("./data/cars.json").then(displayToDOM);
 
-d3.json("./data/cars.json").then(
-  data => (document.getElementById("formatCar").innerText = data.map(formatCar))
-);
+function displayToDOM(data) {
+  document.getElementById("carsData").innerText = JSON.stringify(data, null, 4);
+
+  document.getElementById("formatCar").innerText = data.map(formatCar);
+  document.getElementById("filterCarPrice").innerText = JSON.stringify(
+    filterByPrice(data, 2000)
+  );
+}
